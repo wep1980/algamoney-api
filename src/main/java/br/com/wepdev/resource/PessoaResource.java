8c551649b223b833c1c9acad0bb294f855d19fd2
wep1980 <wepbike@gmail.com>
2022-07-11 20:01:10 -0300

    Atualização completa de Pessoas com validações

--- src/main/java/br/com/wepdev/resource/PessoaResource.java
+++ src/main/java/br/com/wepdev/resource/PessoaResource.java
@@ -5,14 +5,17 @@ import java.util.Optional;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -33,7 +36,6 @@ public class PessoaResource {
 	@Autowired
 	private ApplicationEventPublisher publisher;
 
-	
 	@PostMapping
 	public ResponseEntity<Pessoa> criar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
 		Pessoa pessoaSalva = pessoaRepository.save(pessoa);
@@ -49,13 +51,23 @@ public class PessoaResource {
 		Optional<Pessoa> pessoa = pessoaRepository.findById(codigo);
 		return pessoa.isPresent() ? ResponseEntity.ok(pessoa.get()) : ResponseEntity.notFound().build();
 	}
-	
-	
+
 	@DeleteMapping("/{codigo}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void remover(@PathVariable Long codigo) {
 		this.pessoaRepository.deleteById(codigo);
 	}
-	
+
+	@PutMapping("/{codigo}")
+	public Pessoa atualizar(@PathVariable Long codigo, @Valid @RequestBody Pessoa pessoa) {
+
+		Pessoa pessoaSalva = this.pessoaRepository.findById(codigo)
+				.orElseThrow(() -> new EmptyResultDataAccessException(1));
+		
+        // Copia os dados vindos da requisição "pessoa" e coloca no objeto pessoaSalva, iguinorando na copia o ID
+		BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo");
+
+		return this.pessoaRepository.save(pessoaSalva);
+	}
 
 }
