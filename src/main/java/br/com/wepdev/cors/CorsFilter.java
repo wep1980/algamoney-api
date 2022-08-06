caf237080eca335c0758699f370e5934e16f0045
wep1980 <wepbike@gmail.com>
2022-08-06 12:43:01 -0300

    Criando filtro para CORS

---
+++ src/main/java/br/com/wepdev/cors/CorsFilter.java
@@ -0,0 +1,52 @@
+package br.com.wepdev.cors;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+/**
+ * Filtro criado para a liberação dos CORS
+ * @author wepbi
+ *
+ */
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsFilter implements Filter{
+
+	
+	private String originPermitida = "http://localhost:8000";
+	
+	
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
+			throws IOException, ServletException {
+
+		HttpServletRequest request = (HttpServletRequest) req;
+		HttpServletResponse response = (HttpServletResponse) resp;
+		
+		// Essas 2 configuração estão de fora pq precisam ser sempre enviadas nas requisições
+		response.setHeader("Access-Control-Allow-Origin", originPermitida);
+        response.setHeader("Access-Control-Allow-Credentials", "true"); // Configuração para o cookie que contem o refreshToken ser permitido
+		
+        if ("OPTIONS".equals(request.getMethod()) && originPermitida.equals(request.getHeader("Origin"))) {
+			response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS");
+        	response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
+        	response.setHeader("Access-Control-Max-Age", "3600"); // So depois de 1 hora que o browse vai fazer a proxima requisição
+			
+			response.setStatus(HttpServletResponse.SC_OK);
+		} else {
+			chain.doFilter(req, resp);
+		}
+	}
+
+}
