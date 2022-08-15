6b68141f8fcb39aefc7e43c5263aac9b4f52be61
wep1980 <wepbike@gmail.com>
2022-08-14 21:21:27 -0300

    Endpoint pesquisa de pessoas com paginação

--- src/main/java/br/com/wepdev/repository/PessoaRepository.java
+++ src/main/java/br/com/wepdev/repository/PessoaRepository.java
@@ -1,9 +1,14 @@
 package br.com.wepdev.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.wepdev.model.Pessoa;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Long>{
+	
+	
+	 public Page<Pessoa> findByNomeContaining(String nome, Pageable pageable);
 
 }
